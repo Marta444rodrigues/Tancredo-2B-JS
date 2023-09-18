8d0f8f5488a255d3f3f80c80812781f36740595b
Marta Rodrigues <127526194+Marta444rodrigues@users.noreply.github.com>
2023-09-18 10:44:20 -0300

    AULA 19/09

--- main.js
+++ main.js
@@ -4,23 +4,23 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+ for (let contador = 0; contador < listaDeTeclas.length; contador++){
   
     const tecla = listaDeTeclas[contador];
-
-    const instrumento = listaDeTeclas[contador].classList[1];
-
+    const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
     
     listaDeTeclas[contador].onclick = function () {
         tocaSom(idAudio);
-    }
-
-    contador = contador + 1;
-
-    console.log(contador);
+}
+tecla.onkeydowm = function (evento){
+    if (evento.code === 'Space'|| evento.code === 'Enter'){
+    tecla.classList.add('ativa');}
+}
+tecla.onkeydowm = function () {
+    tecla.classList.remove('ativa');
+  }
 
 }
- 
+
